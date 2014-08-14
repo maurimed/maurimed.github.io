2b86f117e7ebe181f79e59aea21a379db0e547a8
jgarciac <jgarciac87@gmail.com>
2014-08-14 16:52:22 -0500

    Add Ambassadors View Composer

---
+++ app/Epro360/Composers/AmbassadorsComposer.php
@@ -0,0 +1,34 @@
+<?php  namespace Epro360\Composers; 
+
+use Epro360\Repos\Locations\CountriesRepository;
+use Epro360\Repos\Users\Ambassadors\AmbassadorRepository;
+
+class AmbassadorsComposer {
+
+    protected $ambassadorsRepo;
+    /**
+     * @var CountriesRepository
+     */
+    protected $countriesRepo;
+
+    /**
+     * @param AmbassadorRepository $ambassadorsRepo
+     * @param CountriesRepository $countriesRepo
+     */
+    function __construct(AmbassadorRepository $ambassadorsRepo, CountriesRepository $countriesRepo)
+    {
+        $this->ambassadorsRepo = $ambassadorsRepo;
+        $this->countriesRepo = $countriesRepo;
+    }
+
+
+    public function compose($view)
+    {
+        $countries = $this->countriesRepo->getCountriesThatHasAmbassadors();
+        $ambassadors = $this->ambassadorsRepo->getAll();
+
+        $view->withAmbassadors($ambassadors)->withCountries($countries);
+    }
+
+
+} 
