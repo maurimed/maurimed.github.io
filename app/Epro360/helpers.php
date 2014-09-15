5d9597a9cf516e449c8a19ec62239456722f4b77
jgarciac <jgarciac87@gmail.com>
2014-09-15 09:56:43 -0500

    Update helpers functions

--- app/Epro360/helpers.php
+++ app/Epro360/helpers.php
@@ -18,94 +18,28 @@ function setActive($path)
         return Request::is('/') ? 'active': '';
     }
 
+    return '';
 
-};
-
-function get_profile_image($user, $size = 'thumb')
-{
-    if($user->image === "default" )
-    {
-        return '<img class="img-responsive" src="/admin/images/profiles/default.jpg" alt="'. $user->username .'" />';
-    }
-    else
-    {
-        return '<img class="img-responsive"  src="/admin/images/profiles/'. folder_path($user) .'/'. $size . '-' . $user->image .'" alt="'. $user->username .'">';
-    }
 }
 
-function get_profile_image_for_background($user, $size = 'sm')
-{
-    if($user->image === "default" )
-    {
-        return '<img width="100" src="/admin/images/profiles/default.jpg" alt="'. $user->username .'" />';
-    }
-    else
-    {
-        return '<img width="100" src="/admin/images/profiles/'. folder_path($user) .'/'. $size . '-' . $user->image .'" alt="'. $user->username .'">';
-    }
-}
 
 /**
  * @param $user
  * @return string
  */
-function folder_path($user)
+function path($user)
 {
     return strtolower(str_plural($user->userable_type));
 }
 
+function pluralUserType($user)
+{
+    return str_plural($user->userable_type);
+}
+
 function link_to_edit_profile($user)
 {
-    return '<a id="edit-profile" href="/dashboard/users/'. $user->username .'/profile/edit" class="pull-right btn txt-color-white bg-color-green btn-sm"><i class="fa fa-pencil"></i> Edit Profile</a>';
-}
-
-/**
- * @return bool
- */
-function is_admin()
-{
-    return Auth::user()->userable_type == 'Administrator';
-}
-
-/**
- * @return bool
- */
-function is_student()
-{
-    return Auth::user()->userable_type == 'Student';
-}
-
-/**
- * @return bool
- */
-function is_ambassador()
-{
-    return Auth::user()->userable_type == 'Ambassador';
-}
-
-/**
- * @return bool
- */
-function is_parent()
-{
-    return Auth::user()->userable_type == 'Representative';
-}
-
-
-/**
- * @return bool
- */
-function is_director()
-{
-    return Auth::user()->userable_type == 'Director';
-}
-
-/**
- * @return bool
- */
-function is_manager()
-{
-    return Auth::user()->userable_type == 'Manager';
+    return '<a id="edit-profile" href="/dashboard/users/'. $user->username .'/profile/edit" class="pull-right btn txt-color-white bg-color-red btn-sm"><i class="fa fa-pencil"></i> Edit Profile</a>';
 }
 
 
@@ -129,4 +63,15 @@ function canSeeDirector($directorId)
         return false;
 
     return true;
+}
+function get_profile_image_for_background($user, $size = 'sm')
+{
+    if($user->image === "default" )
+    {
+        return '<img width="100" src="/admin/images/profiles/default.jpg" alt="'. $user->username .'" />';
+    }
+    else
+    {
+        return '<img width="100" src="/admin/images/profiles/'. path($user) .'/'. $size . '-' . $user->image .'" alt="'. $user->username .'">';
+    }
 }
