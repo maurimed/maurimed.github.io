c27e755ee7bcf4ff7a74fe9a7913716b68492712
jgarciac <jgarciac87@gmail.com>
2014-09-15 21:13:33 -0500

    Update session messages partial

--- app/views/sessions/messages.blade.php
+++ app/views/sessions/messages.blade.php
@@ -1,35 +1,40 @@
-@if(Session::has('danger_message'))
-    <div class="alert alert-icon alert-danger">
-        <i class="fa fa-exclamation-circle pull-left"></i>
-        <div class="text">
-            <span class="bold">Error ! </span> {{ Session::get('danger_message') }}
-        </div>
-    </div>
-@endif
-@if(Session::has('info_message'))
-    <div class="alert alert-icon alert-info">
-        <i class="fa fa-thumbs-up pull-left"></i>
-        <div class="text">
-            <span class="bold">Thumbs Up ! </span> {{ Session::get('info_message') }}
-        </div>
-    </div>
-@endif
-
 @if(Session::has('warning_message'))
-    <div class="alert alert-icon alert-warning">
-        <i class="fa fa-exclamation-triangle pull-left"></i>
-        <div class="text">
-            <span class="bold">Warning ! </span> {{ Session::get('warning_message') }}
-        </div>
+    <div class="alert alert-warning fade in">
+        <button class="close" data-dismiss="alert"> × </button>
+        <i class="fa-fw fa fa-warning"></i>
+        {{ Session::get('warning_message') }}
     </div>
 @endif
 
 @if(Session::has('success_message'))
-    <div class="alert alert-icon alert-success">
-        <i class="fa fa-info pull-left"></i>
-        <div class="text">
-            <span class="bold">Success ! </span> {{ Session::get('success_message') }}
-        </div>
+    <div class="alert alert-success fade in">
+        <button class="close" data-dismiss="alert"> × </button>
+        <i class="fa-fw fa fa-check"></i>
+        {{ Session::get('success_message') }}
     </div>
 @endif
+
+@if(Session::has('info_message'))
+    <div class="alert alert-info fade in">
+        <button class="close" data-dismiss="alert"> × </button>
+        <i class="fa-fw fa fa-info"></i>
+        {{ Session::get('info_message') }}
+    </div>
+@endif
+
+@if(Session::has('danger_message'))
+    <div class="alert alert-danger fade in">
+        <button class="close" data-dismiss="alert"> × </button>
+        <i class="fa-fw fa fa-times"></i>
+        {{ Session::get('danger_message') }}
+    </div>
+@endif
+
+
+
+
+
+
+
+
 
