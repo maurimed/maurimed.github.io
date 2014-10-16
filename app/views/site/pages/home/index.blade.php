9103dd932c8bbf8a06360f56b4975387b5c6a6c9
jgarciac <jgarciac87@gmail.com>
2014-10-15 22:03:42 -0500

    Uncomment landing form js funcionality

--- app/views/site/pages/home/index.blade.php
+++ app/views/site/pages/home/index.blade.php
@@ -30,7 +30,7 @@
     @if($currentUser)
 
         @include('site.pages.home.partials.services')
-        
+
     @endif
 @stop
 
@@ -46,66 +46,66 @@
 <script src="/backend/js/libs/angular.min.js"></script>
 <script>
     $('document').ready(function(){
-//        $('.loading-locations').css('display','block');
-//        $.ajax({
-//            type: "GET",
-//            url: '/api/ipinfo'
-//        }).done(function(data) {
-//            $('#countries').hide().html(data).slideDown('slow', function(){
-//                $('.loading-locations').css('display','none');
-//            });
-//        });
-//
-//        var $email = $('#e-mail');
-//        var $phone = $('#phone');
-//        $("#landing-form").validate({
-//
-//            rules : {
-//
-//                firstname : { required: true},
-//                email : {
-//                    required : true,
-//                    email : true
-//                },
-//                phone: {
-//                    required: true,
-//                    number: true
-//                },
-//                country : { required: true},
-//                states : { required: true},
-//                cities : { required: true},
-//                interest : { required: true},
-//                find_us : { required: true},
-//                age : { required: true}
-//
-//            },
-//
-//            // Messages for form validation
-//            messages : {
-//                firstname : { required: $('#firstname').data('error')},
-//                email : {
-//                    required : $email.data('error'),
-//                    email : $email.data('invalid')
-//                },
-//                phone : {
-//                    required : $phone.data('error'),
-//                    number: $phone.data('number')
-//                },
-//                country : { required: $('#countries').data('error')},
-//                states : { required: $('#states').data('error')},
-//                cities : { required: $('#cities').data('error')},
-//                interest : { required: $('#interest').data('error')},
-//                find_us : { required: $('#find_us').data('error')},
-//                age : { required: $('#age').data('error')}
-//
-//
-//            },
-//
-//            // Do not change code below
-//            errorPlacement : function(error, element) {
-//                error.insertAfter(element.parent());
-//            }
-//        });
+        $('.loading-locations').css('display','block');
+        $.ajax({
+            type: "GET",
+            url: '/api/ipinfo'
+        }).done(function(data) {
+            $('#countries').hide().html(data).slideDown('slow', function(){
+                $('.loading-locations').css('display','none');
+            });
+        });
+
+        var $email = $('#e-mail');
+        var $phone = $('#phone');
+        $("#landing-form").validate({
+
+            rules : {
+
+                firstname : { required: true},
+                email : {
+                    required : true,
+                    email : true
+                },
+                phone: {
+                    required: true,
+                    number: true
+                },
+                country : { required: true},
+                states : { required: true},
+                cities : { required: true},
+                interest : { required: true},
+                find_us : { required: true},
+                age : { required: true}
+
+            },
+
+            // Messages for form validation
+            messages : {
+                firstname : { required: $('#firstname').data('error')},
+                email : {
+                    required : $email.data('error'),
+                    email : $email.data('invalid')
+                },
+                phone : {
+                    required : $phone.data('error'),
+                    number: $phone.data('number')
+                },
+                country : { required: $('#countries').data('error')},
+                states : { required: $('#states').data('error')},
+                cities : { required: $('#cities').data('error')},
+                interest : { required: $('#interest').data('error')},
+                find_us : { required: $('#find_us').data('error')},
+                age : { required: $('#age').data('error')}
+
+
+            },
+
+            // Do not change code below
+            errorPlacement : function(error, element) {
+                error.insertAfter(element.parent());
+            }
+        });
       $('.select-picker').selectpicker({ size: 1 });
 
 
