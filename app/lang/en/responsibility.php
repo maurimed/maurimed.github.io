fc853933a065cada20fe83862052ca525f36c97c
jgarciac <jgarciac87@gmail.com>
2014-12-01 23:40:55 -0500

    Delete a sentence from social responsibility website section

--- app/lang/en/responsibility.php
+++ app/lang/en/responsibility.php
@@ -9,7 +9,7 @@ return [
 
     "subtitle2" => "A natural drive to do good",
     "p2" => "Epro 360 empowers people through education. Giving access to an education that is beyond the standard, coupled with an irresistible vision and the spirit of social entrepreneurship drives the Epro 360 network. Tom Brokaw, a famous American TV journalist and agent for change, said, “It’s easy to make a bug. It’s hard to make a difference, and it’s so worth it.” This is why Epro 360 Gives evolved.",
-    "p3" => "Epro 360 Gives, which is a global education donation program, runs on a percentage of the annual Epro 360 income. Epro 360 believes that a fundamental education is a basic human right, which is why Epro 360 is working in collaboration with underprivileged communities. To achieve best results, Epro 360 maintains a partnership with the Indian teacher education platform ",
+    "p3" => "Epro 360 Gives, which is a global education donation program, runs on a percentage of the annual Epro 360 income. Epro 360 believes that a fundamental education is a basic human right, which is why Epro 360 is working in collaboration with underprivileged communities.",
 
     "subtitle3" => "A Win-Win Situation",
     "p4" => "Collaboration has become increasingly more important over the past years, because people have realized that one can accomplish more, when working together and building on one’s strengths. This is why Epro 360 believes in “giving an education for receiving an education.”",
